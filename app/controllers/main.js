bc23aef7bf6144da0cd1848b3f3afa7d7d0e84a9
Vladislav <zveronym@gmail.com>
2017-10-19 15:56:00 +0300

    Пример работы с express и nunjucks

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,6 +1,15 @@
+const nunjucks = require('nunjucks');
+
 exports.getPage = (req, res) => {
+    let obj = {}
+    obj.surname = 'Иванов';
     res.render('main.html', {
         name: req.params.name,
-        surname: 'Иванов'
+        obj: obj,
+        items: [
+            'one',
+            'two',
+            'three'
+        ]
     });
 }
