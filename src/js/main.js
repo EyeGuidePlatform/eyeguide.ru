33f376d2fa541f0648bd64c7bfa7b4207fe004b5
Jayzzer <pavel10_pushkarev@mail.ru>
2017-10-22 00:01:31 +0300

    add switch button

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,29 @@
-var menuBtn = document.querySelector('.navbar-toggle');
-var menuList = document.querySelector(menuBtn.dataset.target);
+let menuBtn = document.querySelector('.navbar-toggle');
+let menuList = document.querySelector(menuBtn.dataset.target);
 
-menuBtn.onclick = function () {
+menuBtn.onclick = () => {
     menuList.classList.toggle('in');
-}
+};
+
+
+let switcher = document.querySelector('.switcher');
+switcher.onclick = (e) => {
+    let target = e.target;
+    
+    if (target.tagName != 'BUTTON' || target.classList.contains('active')) return;
+    
+    let disableBtn;
+    switch (target.className) {
+        case 'to-guides':
+            disableBtn = document.querySelector('.to-places');
+
+            break;
+        case 'to-places':
+            disableBtn = document.querySelector('.to-guides');
+
+            break;
+    }
+
+    target.classList.add('active');
+    disableBtn.classList.remove('active');
+};
