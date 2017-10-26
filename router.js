15258c3e38e0f6375cc9ab51404215d8e2ee1450
Jayzzer <pavel10_pushkarev@mail.ru>
2017-10-27 00:29:39 +0300

    separate navbar and footer. fix some errors :)

--- router.js
+++ router.js
@@ -1,14 +1,9 @@
 let express = require('express'),
     router = express.Router(),
-    mainController = require('./app/controllers/main');
+    mainController = require('./app/controllers/main'),
+    mapController = require('./app/controllers/map');
 
 router.get('/', mainController.getHomePage);
-router.get('/place/:id',mainController.getPlacePage);
-router.get('/map', require('./app/controllers/main').getMapPage);
-router.get('/profile/:id', require('./app/controllers/main').getProfile);
-router.get('/gidOptions', mainController.getGidOptionsPage);
-router.get('/gidOrders', mainController.getGidOrdersPage);
-router.get('/gidPlaces', mainController.getGidPlacesPage);
-router.get('/gidProfile/:id', require('./app/controllers/main').getProfilePage);
+router.get('/map', mapController.getMapPage);
 
 module.exports = router;
