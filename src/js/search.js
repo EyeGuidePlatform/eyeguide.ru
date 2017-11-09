2bb8ee354b6d8e6366a897a39e21ff4401384d8b
Bratyhamandarin <bratyhamandarin@gmail.com>
2017-11-09 17:04:27 +0300

    refactoring

--- src/js/search.js
+++ src/js/search.js
@@ -31,26 +31,3 @@ function toggleThis(target) {
             elem.classList.toggle('hidden');
         }, this);
 }
-
-// function switchBlock(target) {
-//     let disableBtn;
-//     switch (target.className) {
-//         case 'to-guides':
-//             disableBtn = document.querySelector('.to-places');
-
-//             break;
-//         case 'to-places':
-//             disableBtn = document.querySelector('.to-guides');
-
-//             break;
-//     }
-
-//     target.classList.add('active');
-//     disableBtn.classList.remove('active');
-
-//     let disableBlock = document.querySelector(disableBtn.dataset.target);
-//     disableBlock.classList.add('hidden');
-
-//     let showBlock = document.querySelector(target.dataset.target);
-//     showBlock.classList.remove('hidden');
-// }
