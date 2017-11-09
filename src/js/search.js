485ffe1b31785cde2de4d60fe044d7cf513781d1
Bratyhamandarin <bratyhamandarin@gmail.com>
2017-11-09 15:04:51 +0300

    fixed buttons

--- src/js/search.js
+++ src/js/search.js
@@ -1,18 +1,18 @@
-let switcher = document.querySelector('.switcher');
+const switcher = document.querySelector('.switcher'),
+      guideOptions = document.querySelectorAll('.search_lang, .search_place, .search_сar')
 switcher.onclick = (e) => {
     let target = e.target;
-    
     if (target.tagName != 'BUTTON' || target.classList.contains('active')) return;
     
     let disableBtn;
     switch (target.className) {
         case 'to-guides':
             disableBtn = document.querySelector('.to-places');
-
+            toggleThis(guideOptions);
             break;
         case 'to-places':
             disableBtn = document.querySelector('.to-guides');
-
+            toggleThis(guideOptions);
             break;
     }
 
@@ -26,26 +26,31 @@ switcher.onclick = (e) => {
     showBlock.classList.remove('hidden');
 };
 
-
-function switchBlock(target) {
-    let disableBtn;
-    switch (target.className) {
-        case 'to-guides':
-            disableBtn = document.querySelector('.to-places');
-
-            break;
-        case 'to-places':
-            disableBtn = document.querySelector('.to-guides');
-
-            break;
-    }
-
-    target.classList.add('active');
-    disableBtn.classList.remove('active');
-
-    let disableBlock = document.querySelector(disableBtn.dataset.target);
-    disableBlock.classList.add('hidden');
-
-    let showBlock = document.querySelector(target.dataset.target);
-    showBlock.classList.remove('hidden');
+function toggleThis(target) {
+        target.forEach( elem => {
+            elem.classList.toggle('hidden');
+        }, this);
 }
+
+// function switchBlock(target) {
+//     let disableBtn;
+//     switch (target.className) {
+//         case 'to-guides':
+//             disableBtn = document.querySelector('.to-places');
+
+//             break;
+//         case 'to-places':
+//             disableBtn = document.querySelector('.to-guides');
+
+//             break;
+//     }
+
+//     target.classList.add('active');
+//     disableBtn.classList.remove('active');
+
+//     let disableBlock = document.querySelector(disableBtn.dataset.target);
+//     disableBlock.classList.add('hidden');
+
+//     let showBlock = document.querySelector(target.dataset.target);
+//     showBlock.classList.remove('hidden');
+// }
