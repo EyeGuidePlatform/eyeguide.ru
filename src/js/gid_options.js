b5650f0ccde00d104c755ba904b3565c644a6d20
Bratyhamandarin <bratyhamandarin@gmail.com>
2017-11-21 12:43:15 +0300

    pass logic

---
+++ src/js/gid_options.js
@@ -0,0 +1,8 @@
+check = (form) => {
+    const elems = form.elements,
+            old_pas = elems.old_pwd.value,
+            new_pas = elems.new_pwd.value,
+            new_pas2 = elems.new_pwd2.value
+    
+
+}
