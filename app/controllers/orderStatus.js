948a9ae0c2ce4effea58a87ded1bc4173af84a8d
Nikita <devilishchuck@gmail.com>
2017-11-21 21:26:43 +0300

    added static methods

--- app/controllers/orderStatus.js
+++ app/controllers/orderStatus.js
@@ -1,3 +1,9 @@
-exports.getOrderStatus = (req, res) => {
+const orderModel = require('../models/orders').orderModel;
+
+
+exports.getOrderStatus = async (req, res) => {
+    let id = req.params.id,
+        orders = await orderModel.getOrders();
+        console.log(orders);
     res.render('orderStatus.html');
 };
