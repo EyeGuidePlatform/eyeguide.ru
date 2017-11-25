7e3885f016912d77dcd273fd81cc8b6b064ae96f
Jayzzer <pavel10_pushkarev@mail.ru>
2017-11-25 23:59:29 +0300

    edit search fields

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -3,13 +3,19 @@
  */
 
 const   placeModel = require('../models/place').placeModel,
-        guideModel = require('../models/guide').guideModel;
+        guideModel = require('../models/guide').guideModel,
+        staticModel = require('../models/static').staticModel;
 
 exports.getSearchPage = async (req, res) => {
-    places = await placeModel.getPlaces({limit: 6})
-    guides = await guideModel.getGuides({limit: 6})
+    const places = await placeModel.getPlaces({limit: 6}),
+          guides = await guideModel.getGuides({limit: 6}),
+          langs = await staticModel.getLangs(req.locale),
+          cities = await staticModel.getCities(req.locale);
+
     res.render('search.html', {
         guides: guides,
-        places: places
+        places: places,
+        langs: langs,
+        cities: cities
     });
 }
--- src/js/search.js
+++ src/js/search.js
@@ -31,3 +31,46 @@ function toggleThis(target) {
             elem.classList.toggle('hidden');
         }, this);
 }
+
+
+//Поле 'выбор мест'
+const placeSelect = new Choices('#places', {
+        removeItemButton: true
+    }),
+
+    citySelect = document.getElementById('city');
+
+(async () => {
+    const places = await getPlacesJSON('city', 'none');
+
+    updatePlaces(places, citySelect.value);
+
+    citySelect.onchange = e => {
+        let newCity = e.target.value;
+
+        updatePlaces(places, newCity);
+    }
+})();
+
+function updatePlaces(places, newCity) {
+    let items = [];
+
+    places.forEach( place => {
+        if (place.city == newCity) {
+            const item = {
+                value: place._id,
+                label: place.name
+            };
+            items.push(item);
+        }
+    } );
+
+    //Обновляем места, принадлежащие выбранному городу
+    placeSelect.clearStore().setChoices(items, 'value', 'label', false);
+}
+
+async function getPlacesJSON (attr, param) {
+    const placesJSON = await $.getJSON('/api/getPlaces/' + attr + '=' + param);
+
+    return JSON.parse( placesJSON );
+}
