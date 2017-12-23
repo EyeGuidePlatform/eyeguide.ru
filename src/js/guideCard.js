06985f3ad7b11387a8756a1b92f0a67c234c8bc8
Nikita <devilishchuck@gmail.com>
2017-12-23 23:19:25 +0300

    fixed multiply spacing

--- src/js/guideCard.js
+++ src/js/guideCard.js
@@ -1,12 +1,8 @@
 for (let i = 1; i <= 3; i++) {
     let children = $('.guideInfo:nth-child(' + i + ') h5'),
-        startText,
-        indexOfComma,
-        resultText;
-    startText = children.text();
-    index = startText.indexOf(',');
-    if (index === -1) continue;
-    resultText = startText.slice(0, index + 1) + ' ' + startText.slice(index + 1, startText.length);
+        info;
+    info = children.text();
+    info = info.replace(/\,/g, ", ");
     children.empty();
-    children.append(resultText);
+    children.append(info);
 }
